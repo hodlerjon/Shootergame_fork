4bbdf3656d4c9c40e0e789c4d071cd45f955d47f
avazcoder <125877678+avazcoder@users.noreply.github.com>
2024-12-03 14:17:59 +0500

    Add files via upload

---
+++ kod.py
@@ -0,0 +1,37 @@
+number = int(input())
+array = list(map(int, input().split()))
+
+even_numbers = []
+odd_number = []
+
+for i in range(len(array)):
+    if i % 2 == 0:
+        even_numbers.append(i)
+
+for i in range(len(array)):
+    if i % 2 == 1:
+        odd_number.append(i)
+        
+for i in range(1, len(even_numbers)):
+    print(even_numbers[i], end = ' ')
+    
+for i in range(1, len(odd_number)):
+    print(odd_number[i], end = ' ')
+    
+        
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
